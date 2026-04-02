2c8fbf621e73454f0c56910df573947587d98143
nook-and-nature <malkinofnookandnature@gmail.com>
2026-04-02 20:58:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post("/webhook", async (req, res) => {
     const from = message.from;
 
     await axios.post(
-      "https://graph.facebook.com/v18.0/YOUR_PHONE_NUMBER_ID/messages",
+      "https://graph.facebook.com/v18.0/1111716828684480/messages",
       {
         messaging_product: "whatsapp",
         to: from,
